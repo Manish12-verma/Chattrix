fd7bf1b5e5a8f2645cea679ddef2a1c0c56b2e23
Manish verma <110876979+Manish12-verma@users.noreply.github.com>
2025-06-01 17:49:12 +0530

    backend setup completed

---
+++ Server/server.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import "dotenv/config";
+import cors from 'cors';
+import http from 'http';
+
+
+//create express app and http server
+const app = express();
+const server = http.createServer(app);
+
+// Middleware setup
+
+app.use(express.json({limit: '4mb'}));
+app.use(cors());
+
+app.use("/api/status", (req, res) => {
+    res.send("Server is running");
+})
+
+
+// Start the server
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+
