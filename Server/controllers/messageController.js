0d3d57a3c0c75c72a282545ef157297ca5891f59
Manish verma <110876979+Manish12-verma@users.noreply.github.com>
2025-06-02 19:22:12 +0530

    created controller to fetch all user

---
+++ Server/controllers/messageController.js
@@ -0,0 +1,39 @@
+import Message from "../models/message.js";
+import User from "../models/User.js";
+
+//Get all user except the logged-in user
+export const getUsersForSidebar = async (req, res) => {
+  try {
+    const userId = req.user._id;
+    const filteredUsers = await User.find({ _id: { $ne: userId } }).select(
+      "-password"
+    );
+
+    //count number of unseen msg
+    const unseenMessages = {};
+    const promises = filteredUsers.map(async (user) => {
+      const messages = await Message.find({
+        senderId: user._id,
+        receiverId: userId,
+        seen: false,
+      });
+      if (messages.length > 0) {
+        unseenMessages[user._id] = messages.length;
+      }
+    });
+    await Promise.all(promises);
+    res.status(200).json({
+      success: true,
+      users: filteredUsers,
+      unseenMessages,
+    });
+  } catch (error) {
+     console.log("Error in getUsersForSidebar:", error);
+    res.status(500).json({
+      success: false,
+      message: error.message,   
+    });
+  }
+};
+ 
+//Get all messages between two users
