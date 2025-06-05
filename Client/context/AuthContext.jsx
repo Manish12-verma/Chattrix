a8d3f31ef3006242398ce5cd16c9d5aa59231ad5
Manish verma <110876979+Manish12-verma@users.noreply.github.com>
2025-06-05 19:25:54 +0530

    added login/signup function

---
+++ Client/context/AuthContext.jsx
@@ -0,0 +1,92 @@
+import { createContext, useEffect, useState } from 'react';
+import axios from 'axios';
+import toast from 'react-hot-toast';
+import { io } from 'socket.io-client';
+
+
+const backendUrl = import.meta.env.VITE_BACKEND_URL;
+axios.defaults.baseURL = backendUrl;
+
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({children})=>{
+
+     const [token, setToken] = useState(localStorage.getItem('token'));
+     const [authUser, setAuthUser] = useState(null);
+     const [onlineUsers, setOnlineUsers] = useState([]);
+     const [socket, setSocket] = useState(null);
+
+    //check if the user is authenticated and if so, set the user data and connect to the socket
+    const checkAuth = async () => {
+      try {
+          const {data} = await axios.get('/api/auth/check');
+          if(data.success){
+            setAuthUser(data.user);
+            connectSocket(data.user);
+          }
+      }catch (error) {
+          toast.error(error.message);
+      }
+    }
+    
+    //Login function to handle user authentication and socket connection
+    const login = async(state,credentials)=>{
+         try {
+             const {data} = await axios.post(`/api/auth/${state}`, credentials);
+             if(data.success){
+              setAuthUser(data.user);
+              connectSocket(data.user);
+              axios.defaults.headers.common['token'] = data.token;
+              setToken(data.token);
+              localStorage.setItem('token', data.token);
+              toast.success(data.message);
+             }else{
+              toast.error(data.message);
+             }
+         } catch (error) {
+              toast.error(error.message);
+            
+         }
+    }
+
+
+
+
+    //connect to the socket and listen for online users
+    const connectSocket =(userData)=>{
+        if(!userData || socket?.connected) return;
+         
+        const newSocket = io(backendUrl, {
+           query: {
+             userId: userData._id,
+           },
+        })
+
+        newSocket.connect();
+        setSocket(newSocket);
+
+        newSocket.on('getOnlineUsers', (userIds) => {
+            setOnlineUsers(userIds);
+            console.log('Socket connected:', newSocket.id);
+        });
+
+    }
+   
+    useEffect(()=>{
+        if(token){
+          axios.defaults.headers.common['token'] = token;
+        }
+        checkAuth();
+    },[])
+
+
+      const value ={
+        axios,authUser,onlineUsers,socket
+      }
+        return (
+                <AuthContext.Provider value={value}>
+                    {children}
+                </AuthContext.Provider>
+        ); 
+};
--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -3,10 +3,12 @@ import './App.css'
 import HomePage from './pages/HomePage'
 import Login from './pages/Login'
 import Profile from './pages/Profile'
+import {Toaster} from 'react-hot-toast'
 
 const App = () => {
   return (
     <div className="bg-[url('./src/assets/bgImage.jpg')] bg-contain">
+        <Toaster/>
         <Routes>
            <Route path='/' element={<HomePage/>}/>
            <Route path='/login' element={<Login/>}/>
